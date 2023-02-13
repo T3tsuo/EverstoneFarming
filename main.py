e33f2e0d7402b3ccd6bbc0f1b0d8d19b26ec4ac4
Takumi Comeau <tk565319@dal.ca>
2023-02-13 17:26:14 -0400

    added feature where user can input how long the code should run for (in seconds)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 import grab_items
 import heal_return
@@ -9,11 +10,18 @@ import heal_return
 os.environ['REQUESTS_CA_BUNDLE'] = "certifi/cacert.pem"
 
 try:
-    # try to grab input from command line
+    # try to grab input from command line for amount of sweet scent
     x = int(sys.argv[1])
+    # try to grab how many seconds to run the code
+    run_code_time = int(sys.argv[2])
 except IndexError:
     # else then ask for amount of times user can use sweet scent before going to pokecenter
     x = int(input("Number of times to use sweet scent: "))
-while True:
+    # ask how long the code should run in seconds
+    run_code_time = int(input("How long (in seconds) do you want the code to run for: "))
+# this is when the code should stop
+end_time = time.time() + run_code_time
+# while we have not reached the end time, keep on going
+while end_time > time.time():
     grab_items.run(x)
     heal_return.run()
