cc00c3f9b2972928f0b1e411a90f88bd87a6662b
Takumi Comeau <tk565319@dal.ca>
2023-03-11 19:46:49 -0400

    Now EverstoneFarming.py only runs when the run function is called

--- EverstoneFarming.py
+++ EverstoneFarming.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import os
 
@@ -10,28 +9,24 @@ import time_to_seconds
 
 os.environ['REQUESTS_CA_BUNDLE'] = "certifi/cacert.pem"
 
-try:
-    # try to grab input from command line for amount of sweet scent
-    x = int(sys.argv[1])
-    # try to grab how many seconds to run the code
-    run_code_time = time_to_seconds.sum_seconds(sys.argv[2])
-except IndexError:
+
+def run():
     # else then ask for amount of times user can use sweet scent before going to pokecenter
     x = int(input("Number of times to use sweet scent: "))
     # ask how long the code should run
     print("Time format: hours.minutes\n2.35 = 2 hours and 35 minutes")
     run_code_time = time_to_seconds.sum_seconds(input("How long do you want the code to run for: "))
-print("Running for " + str(run_code_time) + " seconds")
-time.sleep(2)
-# this is when the code started to run
-start_time = time.time()
-# this is when the code should stop
-end_time = time.time() + run_code_time
-# while we have not reached the end time, keep on going
-while end_time > time.time():
-    heal_return.run()
-    grab_items.run(x)
-    if end_time > time.time():
-        print(str(round((time.time() - start_time) / (end_time - start_time) * 100)) + "% Done")
-    else:
-        print("100% Done")
+    print("Running for " + str(run_code_time) + " seconds")
+    time.sleep(2)
+    # this is when the code started to run
+    start_time = time.time()
+    # this is when the code should stop
+    end_time = time.time() + run_code_time
+    # while we have not reached the end time, keep on going
+    while end_time > time.time():
+        heal_return.run()
+        grab_items.run(x)
+        if end_time > time.time():
+            print(str(round((time.time() - start_time) / (end_time - start_time) * 100)) + "% Done")
+        else:
+            print("100% Done")
